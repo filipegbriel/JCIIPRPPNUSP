9b55e04d2d5cfd132bdadf5ea28a97abc9d10c3d
filipegbriel <filipegbriel@gmail.com>
2017-11-14 15:59:26 -0200

    david esse é o ultimo

--- Azimutal.cpp
+++ Azimutal.cpp
@@ -24,5 +24,5 @@ Azimutal::Azimutal(int confPassos, int pinPasso1, int pinPasso2,
 	int pinRX1, int pinRX2, int pinRX3,
 	int pinRX4) 
 {
-	mtrPasso = Stepper(confPassos, pinPasso1, pinPasso2);
+	mtrPasso = new Stepper(confPassos, pinPasso1, pinPasso2);
 }
--- Azimutal.h
+++ Azimutal.h
@@ -47,23 +47,7 @@ class Azimutal{
     int version(void);
 
   private:
-    void stepMotor(int this_step);
-
-    int direction;            // Direction of rotation
-    unsigned long step_delay; // delay between steps, in ms, based on speed
-    int number_of_steps;      // total number of steps this motor can take
-    int pin_count;            // how many pins are in use.
-    int step_number;          // which step the motor is on
-
-    // motor pin numbers:
-    int motor_pin_1;
-    int motor_pin_2;
-    int motor_pin_3;
-    int motor_pin_4;
-    int motor_pin_5;          // Only 5 phase motor
-
-    unsigned long last_step_time; // time stamp in us of when the last step was taken
-};
+	  Stepper mtrPasso;
 
 #endif
 
