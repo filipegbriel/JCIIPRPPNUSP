81f3e0578a1382675610e1f957322e216b912a65
filipegbriel <filipegbriel@gmail.com>
2017-11-15 23:37:07 -0200

    Abrir com projeto no visual Studio
    OBS: david voce ta com o programa errado, o seu é o visual studio code, não falei nada na hora porque fiquei com preguiça

---
+++ Azimutal/Azimutal.cpp
@@ -0,0 +1,27 @@
+﻿#include "Azimutal.h"
+/*
+ * Azimutal.h - Biblioteca de um azimutal  - Vers�o 1.1
+ *
+ * Funcionamento original	(0.0)   por Filipe Gabriel Santos e Henrique Martinez Rocamora.
+ * Biblioteca para arduino	(0.1)	por Filipe Santos, David Engelstein, Gustavo Takashi, Paulo Yamabuchi
+ *
+ * Esta biblioteca � para o uso privado da institui��o hoje (2017) nomeada por PoliN�utico - grupo de
+ * extens�o acad�mica da Escola Politecninca da Universidade de S�o Paulo. Seu uso n�o est� autorizado 
+ * para qualquer outra pessoa que n�o seja, ou n�o tenha sido, associada ao grupo.
+ *
+ * Universidade de S�o Paulo, S�o Paulo, S�o Paulo, Brasil. Novembro de 2017
+ * 
+ * Esta biblioteca controla um motor de passo ligado a uma propuls�o azimutal do barco Jo�o C�ndido II.
+ * Sua comunica��o com o receptor usa 3 canais e um sensor de fim de curso, o qual verifica se o barco est�
+ * em zero com a normal.
+ */
+
+
+Azimutal::Azimutal(int pinRX[0], int pinRX[1], int pinRX[2], int pinRX[3])
+{
+}
+
+
+Azimutal::~Azimutal()
+{
+}
